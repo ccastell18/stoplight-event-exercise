fdebfe4e56542ee1b7f8e6a41dba1e01c3a0207b
Christopher Castellano <c.castellano18@gmail.com>
2017-11-09 10:54:00 -0600

    https://github.com/ccastell18/stoplight-event-exercise

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-(function() {
-  'use strict';
+let controls =  document.getElementById('controls');
+controls.addEventListener('click', function(event){
+  let signal = event.target.id.replace('Button', '');
+
+ document.getElementById(signal + 'Light').classList.toggle(signal);
+});
+
+
+
 
   // YOUR CODE HERE
-})();
